7bf628f44cc9ac4013b8719cc23947b1db27bee0
carver.mdg <carver.mdg@gmail.com>
2025-05-26 23:04:24 +0300

    refactor: data loading in AdminSysComponent to use a unified method

--- src/app/pages/admin-sys/admin-sys.component.ts
+++ src/app/pages/admin-sys/admin-sys.component.ts
@@ -12,6 +12,8 @@ import { UserTabItemComponent } from './components/user-tab-item/user-tab-item.c
 import { AdminSysService, DepartmentService, EmployeeService, ServiceSysService, StaffUnitService, UserService } from './services';
 import { PageState } from './state/PageState';
 import { StateLoadingItem } from './state/types';
+import { Observable } from 'rxjs';
+import { DepartmentModel, EmployeeModel, ServiceSysModel, StaffUnitModel, UserModel } from 'app/models';
 
 @Component({
   selector: 'app-admin-sys',
@@ -20,7 +22,7 @@ import { StateLoadingItem } from './state/types';
     MatTabsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule,
     DepartmentTabItemComponent, StaffUnitTabItemComponent, EmployeeTabItemComponent, UserTabItemComponent,
     ServiceSysTabItemComponent
-],
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './admin-sys.component.html',
   styleUrl: './admin-sys.component.scss'
@@ -51,32 +53,103 @@ export class AdminSysComponent implements OnInit {
    * Lifecycle hook that is called after Angular has initialized
    */
   ngOnInit() {
-    this.loadDeparatments();
-    this.loadStaffUnits();
-    this.loadEmployees();
-    this.loadUsers();
-    this.loadServicesSys();
-    
+    this.loadData();
+  }
 
-    // @COMMENT How rewrite several load functions to single code bellow ?
-    /*
-        // Load data
-        [
-          { stateItem: this.pageState.departments, loaderItem: this.departmentService.loadList() },
-          { stateItem: this.pageState.staffUnits, loaderItem: this.staffUnitService.loadList() },
-          { stateItem: this.pageState.employees, loaderItem: this.employeeService.loadList() },
-          { stateItem: this.pageState.users, loaderItem: this.userService.loadList() },
-          { stateItem: this.pageState.servicesSys, loaderItem: this.serviceSysService.loadList() },
-        ].forEach(item => {
-          item.stateItem.loadingState.set(StateLoadingItem.loading());
 
-          item.loaderItem.subscribe({
-            next: data => item.stateItem.create(data),
-            error: (err) => item.stateItem.loadingState.set(StateLoadingItem.error(err)),
-            complete: () => item.stateItem.loadingState.set(StateLoadingItem.complete())
-          });
-        });
-    */
+  /**
+   * Loads data for various state items in the admin system component.
+   *
+   * This method initializes the loading process for multiple loadable items, such as departments,
+   * staff units, employees, users, and service systems. Each item is represented as an `ILoadableItem`
+   * containing its loading state and observable loader. The method ensures that the loading state
+   * is updated appropriately during the loading lifecycle (loading, error, complete).
+   *
+   * @throws {Error} If an unknown state item type is encountered.
+   */
+  private loadData() {
+    /**
+     * Represents a loadable item with a specific type `T`, providing mechanisms for managing its loading state
+     * and creating new instances of the item.
+     *
+     * @template T - The type of the item being loaded and managed.
+     *
+     * @property stateItem - An object containing methods to manage the loading state and create new items.
+     * @property stateItem.loadingState.set - A method to set the loading state of the item.
+     * @property stateItem.create - A method to create a new instance of the item with the provided data.
+     *
+     * @property loaderItem - An observable that emits the loaded item of type `T`.
+     */
+    interface ILoadableItem<T> {
+      stateItem: {
+        loadingState: { set: (state: StateLoadingItem) => void };
+        create: (data: T) => void;
+      };
+      loaderItem: Observable<T>;
+    }
+
+    /**
+     * Loads an item by updating its loading state and subscribing to its loader observable.
+     * 
+     * @template T - The type of the data being loaded.
+     * @param item - The loadable item containing the state and loader observable.
+     * @param item.stateItem - The state management object for the item.
+     * @param item.stateItem.loadingState - The state object that tracks the loading state.
+     * @param item.loaderItem - The observable that emits the data to be loaded.
+     */
+    function loadItem<T>(item: ILoadableItem<T>) {
+      item.stateItem.loadingState.set(StateLoadingItem.loading());
+      item.loaderItem.subscribe({
+        next: (data: T) => item.stateItem.create(data),
+        error: (err: any) => item.stateItem.loadingState.set(StateLoadingItem.error(err)),
+        complete: () => item.stateItem.loadingState.set(StateLoadingItem.complete())
+      });
+    }
+
+    /**
+     * Represents a union type of possible loadable models for the admin system component.
+     * Each model is wrapped in an `ILoadableItem` to indicate its loadable state.
+     */
+    type TLoadModelPossibles =
+      ILoadableItem<DepartmentModel[]> |
+      ILoadableItem<StaffUnitModel[]> |
+      ILoadableItem<EmployeeModel[]> |
+      ILoadableItem<UserModel[]> |
+      ILoadableItem<ServiceSysModel[]>;
+
+    // load data
+    [
+      { stateItem: this.pageState.departments, loaderItem: this.departmentService.loadList() } as ILoadableItem<DepartmentModel[]>,
+      { stateItem: this.pageState.staffUnits, loaderItem: this.staffUnitService.loadList() } as ILoadableItem<StaffUnitModel[]>,
+      { stateItem: this.pageState.employees, loaderItem: this.employeeService.loadList() } as ILoadableItem<EmployeeModel[]>,
+      { stateItem: this.pageState.users, loaderItem: this.userService.loadList() } as ILoadableItem<UserModel[]>,
+      { stateItem: this.pageState.servicesSys, loaderItem: this.serviceSysService.loadList() } as ILoadableItem<ServiceSysModel[]>
+    ].forEach((item: TLoadModelPossibles) => {
+      switch (item.stateItem) {
+        case this.pageState.departments:
+          loadItem(item as ILoadableItem<DepartmentModel[]>);
+          break;
+
+        case this.pageState.staffUnits:
+          loadItem(item as ILoadableItem<StaffUnitModel[]>);
+          break;
+
+        case this.pageState.employees:
+          loadItem(item as ILoadableItem<EmployeeModel[]>);
+          break;
+
+        case this.pageState.users:
+          loadItem(item as ILoadableItem<UserModel[]>);
+          break;
+
+        case this.pageState.servicesSys:
+          loadItem(item as ILoadableItem<ServiceSysModel[]>);
+          break;
+
+        default:
+          throw new Error('Load data: Unknown state item type');
+      } // /switch (item.stateItem)
+    });
   }
 
 
@@ -102,76 +175,6 @@ export class AdminSysComponent implements OnInit {
   public onClickTruncateDb() {
     this.adminSysService.truncateDb().subscribe({
       next: () => window.location.reload()
-    });
-  }
-
-
-  /**
-   * Load departments with service
-   */
-  private loadDeparatments() {
-    this.pageState.departments.loadingState.set(StateLoadingItem.loading());
-
-    this.departmentService.loadList().subscribe({
-      next: departments => this.pageState.departments.create(departments),
-      error: (err) => this.pageState.departments.loadingState.set(StateLoadingItem.error(err)),
-      complete: () => this.pageState.departments.loadingState.set(StateLoadingItem.complete())
-    });
-  }
-
-
-  /**
-   * Load staff units with service
-  */
-  private loadStaffUnits() {
-    this.pageState.staffUnits.loadingState.set(StateLoadingItem.loading());
-
-    this.staffUnitService.loadList().subscribe({
-      next: staffUnits => this.pageState.staffUnits.create(staffUnits),
-      error: (err) => this.pageState.staffUnits.loadingState.set(StateLoadingItem.error(err)),
-      complete: () => this.pageState.staffUnits.loadingState.set(StateLoadingItem.complete())
-    });
-  }
-
-
-  /**
-   * Load employees with service
-  */
-  private loadEmployees() {
-    this.pageState.employees.loadingState.set(StateLoadingItem.loading());
-
-    this.employeeService.loadList().subscribe({
-      next: employees => this.pageState.employees.create(employees),
-      error: (err) => this.pageState.employees.loadingState.set(StateLoadingItem.error(err)),
-      complete: () => this.pageState.employees.loadingState.set(StateLoadingItem.complete())
-    });
-  }
-
-
-  /**
-   * Load users with service
-  */
-  private loadUsers() {
-    this.pageState.users.loadingState.set(StateLoadingItem.loading());
-
-    this.userService.loadList().subscribe({
-      next: users => this.pageState.users.create(users),
-      error: (err) => this.pageState.users.loadingState.set(StateLoadingItem.error(err)),
-      complete: () => this.pageState.users.loadingState.set(StateLoadingItem.complete())
-    });
-  }
-
-
-  /**
-  * Load system services with service
-  */
-  private loadServicesSys() {
-    this.pageState.servicesSys.loadingState.set(StateLoadingItem.loading());
-
-    this.serviceSysService.loadList().subscribe({
-      next: services => this.pageState.servicesSys.create(services),
-      error: (err) => this.pageState.servicesSys.loadingState.set(StateLoadingItem.error(err)),
-      complete: () => this.pageState.servicesSys.loadingState.set(StateLoadingItem.complete())
     });
   }
 }
